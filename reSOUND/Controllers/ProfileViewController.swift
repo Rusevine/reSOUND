0e18d0d96f2bbfa65e1653ae5ea9046adf7e8f48
Kyla <kylag44@gmail.com>
2018-09-20 19:51:22 -0400

    added textfields to profile view controller

--- reSOUND/Controllers/ProfileViewController.swift
+++ reSOUND/Controllers/ProfileViewController.swift
@@ -10,26 +10,21 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
-    override func viewDidLoad() {
+  @IBOutlet weak var profileView: UIView!
+  @IBOutlet weak var nameTextField: UITextField!
+  @IBOutlet weak var cityTextField: UITextField!
+  @IBOutlet weak var provinceTextField: UITextField!
+  @IBOutlet weak var skillsTextField: UITextField!
+  @IBOutlet weak var emailTextField: UITextField!
+  
+  override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
