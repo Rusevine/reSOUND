4055356c62f913c813cd8e0a7176c3ad9b7ea068
Kyla <kylag44@gmail.com>
2018-09-21 14:05:23 -0400

    added chat controller and cell

---
+++ reSOUND/Controllers/ChatCell.swift
@@ -0,0 +1,27 @@
+//
+//  ChatCell.swift
+//  reSOUND
+//
+//  Created by Kyla  on 2018-09-21.
+//  Copyright © 2018 Kyla . All rights reserved.
+//
+
+import UIKit
+
+class ChatCell: UITableViewCell {
+  
+  @IBOutlet weak var chatImageView: UIImageView!
+  @IBOutlet weak var chatLabel: UILabel!
+  
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ reSOUND/Controllers/ChatController.swift
@@ -0,0 +1,28 @@
+//
+//  ChatController.swift
+//  reSOUND
+//
+//  Created by Kyla  on 2018-09-21.
+//  Copyright © 2018 Kyla . All rights reserved.
+//
+
+import UIKit
+
+class ChatController: UIViewController {
+
+  @IBOutlet weak var chatTableView: UITableView!
+  
+  @IBOutlet weak var chatTextField: UITextField!
+  
+  @IBOutlet weak var chatSendButton: UIButton!
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+  @IBAction func chatSendButtonPressed(_ sender: UIButton) {
+  }
+  
+}
