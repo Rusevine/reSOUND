74e27a57c6d256fd59d446882a5acffd393b74aa
Kyla <kylag44@gmail.com>
2018-09-22 13:40:37 -0400

    commited buttons

---
+++ reSOUND/Model/UIViewExtentsion.swift
@@ -0,0 +1,24 @@
+//
+//  UIViewExtentsion.swift
+//  reSOUND
+//
+//  Created by Kyla  on 2018-09-22.
+//  Copyright © 2018 Kyla . All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+  
+  func setGradientBackground(colorOne: UIColor, colorTwo: UIColor) {
+    let gradientLayer = CAGradientLayer()
+    gradientLayer.frame = bounds
+    gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
+    gradientLayer.locations = [0.0, 1.0]
+    gradientLayer.startPoint = CGPoint(x: 1.0, y: 1.0)
+    gradientLayer.endPoint = CGPoint(x: 0.0, y: 0.0)
+    
+    layer.insertSublayer(gradientLayer, at: 0)
+  }
+}
